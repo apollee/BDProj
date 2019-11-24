f1a3e0f69a24cfd2d9772a26b5ee320bb49d93a8
M.Crespo <miguelcampelocrespo@gmail.com>
2019-11-24 21:46:24 +0000

    all done

--- populate.py
+++ populate.py
@@ -1,8 +1,9 @@
 import random
+from datetime import datetime
 import linecache
 
 postgres_box_str = "( ( %d , %d ) , ( ( %d , %d ) )"
-
+qualified_emails = []
 def sqlBox(box):
     return postgres_box_str % (box[0][0] , box[0][1], box[1][0], box[1][1])
 
@@ -33,13 +34,21 @@ def popAnomalia(identifier, box, bytea , lingua, desc, tem_anom_red, fds):
 def popAnomaliaTrad(identifier, box, l2, fds):
     insertTable("anomalia_traducao", [identifier, sqlBox(box), sqlString(l2)], fds)
 
+def popPropCorrecao(email, nro, timestamp, desc, fds):
+    insertTable("proposta_de_correcao", [email, nro, timestamp, desc], fds)
 def popUsers(email, password, nUsers, fds):
     insertTable('utilizador', [email, password], fds)
     if (nUsers % random.randint(1,10)) == 0: #add some randomness
         insertTable('utilizador_qualificado', [email], fds)
+        qualified_emails.append(email)
     else:
         insertTable('utilizador_regular', [email], fds)
 
+def popIncidencia(aID, iID, email, fds):
+    insertTable('incidencia', [str(aID), str(iID), sqlString(email)], fds)
+
+def popCorrecao(email, nro, aID, fds):
+    insertTable('correcao', [email, nro, aID], fds)
 def sqlString(string):
     return "'" + string + "'"
 
@@ -47,7 +56,9 @@ def sqlString(string):
 def main():
 
     sqlFile = open("populate.sql", "w")
-
+    aID = []
+    iID = []
+    emails = []
     with open("coordinates.csv", "r") as coordinates: #random csv file on the internet
         counter = 1
         for line in coordinates:
@@ -58,6 +69,7 @@ def main():
             popLocalPublico(lat, longi, name , sqlFile)
             if(counter % random.randint(1,3) == 0):
                 popItem(counter, sqlString(str(linecache.getline('descriptions.txt', counter))[:-1]), name, lat, longi, sqlFile)
+                iID.append(counter)
                 counter+= 1
         sqlFile.write('\n')
 
@@ -68,6 +80,7 @@ def main():
             data = line.split(",")
             data[1] = data[1][:-1] #remove \n
             popUsers(sqlString(data[0]), sqlString(data[1]), nUsers, sqlFile)
+            emails.append(data[0])
             nUsers+=1
         sqlFile.write('\n')
 
@@ -83,6 +96,7 @@ def main():
             for desc in descriptions:
                 lingua_1 = random.choice(languages)
                 popAnomalia(str(counter), ((10, 10), (20, 20)), "", lingua_1,desc[:-1], "TRUE", sqlFile)
+                aID.append(counter)
                 if((counter % 6) == 0):
                     lingua_2 = random.choice(languages)
                     while(lingua_2 == lingua_1):
@@ -90,12 +104,16 @@ def main():
                     popAnomaliaTrad(str(counter), ((20 , 20) , (30, 30)), lingua_2, sqlFile)
                 counter +=1
 
+    #incidencias
+    for aidentifier in aID:
+        popIncidencia(random.choice(aID), random.choice(iID), random.choice(emails), sqlFile)
 
+    counter = 1
+    for email in qualified_emails:
+        popPropCorrecao(email, str(counter), sqlString(str(datetime.now())), sqlString(str(linecache.getline('descriptions.txt', counter))[:-1]),sqlFile)
+        popCorrecao(email, str(counter), str(random.choice(aID)), sqlFile)
 
-
-
-
-
+        counter += 1
 
 if __name__ == "__main__":
     main()
