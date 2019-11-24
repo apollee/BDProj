c8de797d317cd7028fb601c0fc7cab3d33102804
M.Crespo <miguelcampelocrespo@gmail.com>
2019-11-24 23:49:38 +0000

    drop.sql

---
+++ drop.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS duplicado;
+DROP TABLE IF EXISTS correcao;
+
+DROP TABLE IF EXISTS proposta_de_correcao;
+
+DROP TABLE IF EXISTS incidencia;
+
+DROP TABLE IF EXISTS utilizador_qualificado;
+DROP TABLE IF EXISTS utilizador_regular;
+DROP TABLE IF EXISTS utilizador;
+
+
+DROP TABLE IF EXISTS anomalia_traducao;
+DROP TABLE IF EXISTS anomalia;
+DROP TABLE IF EXISTS item;
+DROP TABLE IF EXISTS local_publico;
