d8e40b71c40bd2c9a43ef3f61927206a13c6bc17
apollee <mariabeatriz.dongamartins@gmail.com>
2019-11-25 23:41:36 +0000

    started and finished the regist incidence page including the php

---
+++ web/incidences_final.php
@@ -0,0 +1,45 @@
+<html>
+      <head>
+            <title>Registar incidências</title>
+            <link rel="stylesheet" href="item.css">
+      </head>
+      <body>
+      <?php
+            $caught = false;
+            try {
+                  $id_anomalia = $_REQUEST['id_anomalia'];
+                  $id_item = $_REQUEST['id_item'];
+                  $email = $_REQUEST['email_incidencia'];
+
+                  $host = "db.ist.utl.pt";
+                  $user = "ist190334";
+                  $password = "123456789";
+                  $dbname = $user;
+
+                  $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+                  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                  
+                  $sql = "INSERT into incidencia (anomalia_id, item_id, email) values (?, ?, ?);";
+
+                  $result = $db->prepare($sql);
+
+                  $result->execute([$id_anomalia, $id_item, $email]);
+
+                  $db = null;
+            }
+            catch (PDOException $e){
+                  $caught = true;
+                  echo("<p>ERROR: {$e->getMessage()}</p>");
+            }
+            if(!$caught){
+                  echo("<h1>Registada a incidência com sucesso!</h1>");
+            }else{
+                  echo("<h1>O registo da incidência falhou.</h1>");
+            }
+      ?>
+       <div>
+            <button onclick="location.href='main.html'" type="button">Home</button>
+      </div>
+      </body>
+</html>
